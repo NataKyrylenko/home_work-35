b2ac9636a1dc0c0cfdc1e42175ffaa74b5017b17
natakyrylenko <natalik10107919@gmail.com>
2022-01-25 20:10:00 +0200

    initial commit

---
+++ main.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $('.slick-slider').slick({
+    infinite:false,  
+    arrows: true,
+    slidesToShow: 2.5,
+    slidesToScroll: 1,
+    prevArrow: '<span class="priv_arrow"><i class="fas fa-angle-left"></i></span>',
+    nextArrow: '<span class="next_arrow"><i class="fas fa-angle-right"></i></span>'
+})
+})
+
